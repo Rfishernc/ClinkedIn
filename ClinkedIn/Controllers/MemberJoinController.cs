c3a98e431fcf875d709ee3a7a1c82c4224792c12
RichFisher <rich.fishernc@gmail.com>
2019-04-09 18:46:36 -0500

    merging

---
+++ ClinkedIn/Controllers/MemberJoinController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Database;
+using ClinkedIn.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberJoinController : ControllerBase
+    {
+        readonly MembersData _Members;
+        readonly MemberJoinValidator _Validator;
+
+        public MemberJoinController()
+        {
+            _Members = new MembersData();
+            _Validator = new MemberJoinValidator();
+        }
+
+        [HttpPost("register")]
+        public ActionResult<int> AddMember(MemberJoinRequest joinRequest)
+    }
+}
---
+++ ClinkedIn/Database/MembersData.cs
@@ -0,0 +1,21 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Database
+{
+    public class MembersData
+    {
+        static List<Member> _Members { get; set; } = new List<Member>();
+        static int _MemberIdCount = 0;
+
+        public void AddNewMember(Member newMember)
+        {
+            _MemberIdCount++;
+            newMember.Id = _MemberIdCount;
+            _Members.Add(newMember);
+        }
+    }
+}
