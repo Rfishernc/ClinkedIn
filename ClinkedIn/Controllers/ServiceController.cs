ba2cfa692db7910f850a0d4bb2f46da06adb9e57
WaysideTester <dylanmurray211@outlook.com>
2019-04-13 10:01:01 -0500

    adding services based on member ID

---
+++ ClinkedIn/Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿using ClinkedIn.Data;
+using ClinkedIn.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        readonly MemberRepo _memberRepo;
+
+        public ServiceController()
+        {
+            _memberRepo = new MemberRepo();
+        }
+
+        [HttpGet]
+        public ActionResult<Member> AddMemberServices(AddService addServiceRequest)
+        {
+            Member currentMemb = _memberRepo.GetMember(addServiceRequest.MemberId);
+
+            currentMemb.Services.AddRange(addServiceRequest.Services);
+
+            return currentMemb;
+        }
+    }
+}
---
+++ ClinkedIn/Models/AddService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class AddService
+    {
+        public int MemberId { get; set; }
+        public List<string> Services { get; set; }
+    }
+}
